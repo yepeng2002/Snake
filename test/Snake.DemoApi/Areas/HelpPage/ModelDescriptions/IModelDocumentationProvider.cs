eabaf6abb2448149a2d75c50e9599f07eb20834e
叶鹏 <yepeng2002@sina.com>
2017-10-02 21:09:46 +0800

    Snake Demo of webapi

---
+++ test/Snake.DemoApi/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace Snake.DemoApi.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}

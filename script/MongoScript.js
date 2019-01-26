1b7a60a04057e450f6eb34d1e4a02b9c9795a9da
ExcelMatrix <yepeng2002@sina.com>
2019-01-26 23:59:03 +0800

    固定集合脚本

--- script/MongoScript.js
+++ script/MongoScript.js
@@ -4,4 +4,7 @@ db.createCollection("TrackLog")
 //db.TrackLog.ensureIndex({"RequestTime":1},{expireAfterSeconds:60*60*24*1},{"background":true})
 //db.TrackLog.getIndexes()
 //db.TrackLog.find({Url:"testurl003"}).explain()
-db.TrackLog.remove({ActionName:"Post"})
+//db.TrackLog.remove({ActionName:"Post"})
+
+//固定集合 KB
+//db.createCollection("TrackLog",{capped:true,size:102400,max:100000}); 

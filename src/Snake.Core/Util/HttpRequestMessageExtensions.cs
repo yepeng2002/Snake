78357187cb43108ef64addb4858bc225381646a5
yepeng2002 <yepeng2002@sina.com>
2019-03-22 18:15:25 +0800

    增加根据http请求获取客户端ip方法

---
+++ src/Snake.Core/Util/HttpRequestMessageExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System.Net.Http;
+using System.Text.RegularExpressions;
+
+namespace Snake.Core.Util
+{
+    public static class HttpRequestMessageExtensions
+    {
+        private const string HttpContext = "MS_HttpContext";
+        private const string RemoteEndpointMessage = "System.ServiceModel.Channels.RemoteEndpointMessageProperty";
+        private const string OwinContext = "MS_OwinContext";
+
+        public static string GetClientIpAddress(this HttpRequestMessage request)
+        {
+            // Web-hosting. Needs reference to System.Web.dll
+            if (request.Properties.ContainsKey(HttpContext))
+            {
+                dynamic ctx = request.Properties[HttpContext];
+                if (ctx != null)
+                {
+                    return ctx.Request.UserHostAddress;
+                }
+            }
+
+            // Self-hosting. Needs reference to System.ServiceModel.dll. 
+            if (request.Properties.ContainsKey(RemoteEndpointMessage))
+            {
+                dynamic remoteEndpoint = request.Properties[RemoteEndpointMessage];
+                if (remoteEndpoint != null)
+                {
+                    return remoteEndpoint.Address;
+                }
+            }
+
+            // Self-hosting using Owin. Needs reference to Microsoft.Owin.dll. 
+            if (request.Properties.ContainsKey(OwinContext))
+            {
+                dynamic owinContext = request.Properties[OwinContext];
+                if (owinContext != null)
+                {
+                    return owinContext.Request.RemoteIpAddress;
+                }
+            }
+
+            return null;
+        }
+
+        #region  判断是否是IP格式
+
+        ///  <summary>
+        ///  判断是否是IP地址格式  0.0.0.0
+        ///  </summary>
+        ///  <param  name="str1">待判断的IP地址</param>
+        ///  <returns>true  or  false</returns>
+        public static bool IsIPAddress(string str1)
+        {
+            if (string.IsNullOrEmpty(str1) || str1.Length < 7 || str1.Length > 15) return false;
+
+            const string regFormat = @"^d{1,3}[.]d{1,3}[.]d{1,3}[.]d{1,3}$";
+
+            var regex = new Regex(regFormat, RegexOptions.IgnoreCase);
+            return regex.IsMatch(str1);
+        }
+
+        #endregion
+
+    }
+}
